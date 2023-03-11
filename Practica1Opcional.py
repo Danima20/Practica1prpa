45b332bd0465943e8b2635917e5ef8a18ac5003b
Danima20 <124143288+Danima20@users.noreply.github.com>
2023-03-11 13:36:47 +0100

    Practica opcional final
    Se completa la practica 1, parte opcional con un buffer fijo, (en el codigo 3) y un numero de producciones por productores mayor a él (en el código es 5).
    Se emplean dos listas de indices, una para productores y otra para consumidores, se modifican los empty para ser Semaphore(Tamaño del buffer) ya que hay mas de un solo hueco disponible. Además se hace uso de la función comprobar que se encarga de ver si todos los productores han terminado, en el caso de que al intentar consumir se encuentre con que el storage tenga numeros negativos (se puede dar el caso de que haya -2 y -1 indicando que no se ha acabdo la produccion, por eso es necesaria la función)

--- Practica1Opcional.py
+++ Practica1Opcional.py
@@ -2,49 +2,85 @@
 @author: Daniel Martinez Martin
 """
 
-
 from multiprocessing import Process, Lock, Semaphore, Value, Array
 import random
 
-NPROD = 10
+NPROD = 6
 N = 5# número de elementos que produce cada productor
-K = NPROD * N  # tamaño de la lista compartida
 NInd=3 #numero de elemtnos que tendra el buffer limitado correspondiente a cada productor
-       #este servira para ver cual de los huecos de la lista es un -2
+       
 
 
-def producer(storage, index, empty,non_empty, mutex,nombre,list_indices_prod):
+def producer(storage, index, empty,non_empty,nombre,list_indices_prod,mutex_prod):
+    """
+    la función que funciona como un productor toma tres tipos distintos de semaforos,
+    el empty y el  non-empty que ya estaban en la parte no opcional que señalan si 
+    hay hueco o no y un mutex eclusivo a cada productor.
+    El mutex se encarga de que solo pueda producir un elemento a la vez, ya que necesita
+    marcar la siguiente posicion del buffer cuando pone un elemento. También se ha
+    cambiado el seamforo empty a un Semaphore(NInd) para que señale que al principio
+    tiene tantos huecos libres como la capacidad del buffer.
+    Se usa el inicial=1 para generar los numeros crecientes, ya que cada vuelta de
+    bucle aumenta el valor, la cantidad que aumente puede ser modificada.
+    """
     inicial=1
     for i in range(N):
-        empty.acquire()
+        mutex_prod.acquire()
+        empty.acquire() 
         try:
             nuevo=random.randint(inicial, 10*(i+1))
             storage[list_indices_prod[nombre]%NInd] = nuevo
             print (f"El productor {nombre} esta produciendo: {nuevo} en la \
-                   posiscion {list_indices_prod[nombre]}\n")
-            list_indices_prod[i]+=1
+posiscion {list_indices_prod[nombre]%NInd}\n")
+            list_indices_prod[nombre]+=1
             index.value += 1
             inicial=nuevo
         finally:
             non_empty.release()
+            mutex_prod.release()
     empty.acquire()
-    storage[i]=-2
+    storage[0]=-1
     non_empty.release()
 
 def minimo(lista):
+    """
+    Funcion que se usa para escoger el minimo valor a consumir, devolviendo
+    el valor junto con el productor que lo habia producido.
+    """
     resultado=lista[0]
     for i in lista:
         if i[0]<resultado[0]:
             resultado = i
     return resultado
 
+
+def comprobar(storage):
+    """
+    Esta funcion sirve para comprobar si todos los productores han terminado
+    sus producciones, para ello comprueba que todos hayan puesto el menos uno.
+    """
+    numMUno=0
+    for i in range(NPROD):
+        for k in storage[i]:
+            if k==-1:
+                numMUno+=1
+    if numMUno==NPROD:
+        return True
+    else:
+        return False
+    
 def get_data(storage, index, mutex):
+    """
+    Recorro el storage que tiene el almacen de los productores y usando la lista 
+    de indices a consumir, se meten en una lista y se elije el minimo de ellos.
+    El elegido señala al productor para al que se le avanza +1 en la lista de 
+    indices a consumir.
+    """
     mutex.acquire()
     fin=0
     try:
         lista=[]
-        #Recorro el storage que tiene el almacende los productores y usando la lista de indices que
-        #tiene la posicion del elemento a consumir, los meto en una lista y elijo el minimo
+       
         for i in range(NPROD):
             if index[i]<N:
                 if storage[i][(index[i])%NInd]>=0:
@@ -53,15 +89,23 @@ def get_data(storage, index, mutex):
             data=minimo(lista)
             index[data[1]] = index[data[1]]+1
         else:
-            data=-1
-            fin=1
+            if comprobar(storage):
+                data=-1
+                fin=1
+            else:
+                fin=0
     finally:
         mutex.release()
     return data,fin
 
 
-
 def consumer(storage, mutex,list_ind,storage_final,empty,non_empty):
+    """
+    Se hace uso de una variable auxiliar fin que nos indicara si se ha terminado
+    la produccion. El fin se mantiene igual a 0 si todavia queda algun productor 
+    produciendo y cuando es menos uno significa que la funcion comprobar anterior
+    ha detectado que todos han terminado de producir.
+    """
     for i in non_empty:
         i.acquire()
     fin=0
@@ -78,10 +122,10 @@ el productor {dato[1]}\n")
 
 def main():
     mutex = Lock()
+    mutex_sem = [Semaphore(1) for i in range(NPROD)]
     index = Value('i', 0)
-    #Usaré una lista de inidices pq al tener un tamano fijo para el storage de cada productor
-    #necesitare saber a que indice le corresponde introducir a cada productor, y tb
-    #usare una lista de indices para saber a que 
+    #Una lista de indices para los productores, que sepan donde les toca producir,
+    # y otra para que el consumidor sepa cual tiene que consumir. Se inicia en 0.
     lista_indices_prod=[0]*NPROD #Pasamos a cada productor su indice
     lista_indic_consum=[0]*NPROD
     #NInd=3
@@ -95,7 +139,7 @@ def main():
     storage_final=[]
     prodlst = [Process(target=producer,
                        name=f'prod_{i}',
-                       args=(storage[i],index,empty[i],non_empty[i],mutex,i,lista_indices_prod))
+                       args=(storage[i],index,empty[i],non_empty[i],i,lista_indices_prod,mutex_sem[i]))
                for i in range(NPROD)]
 
     cons = Process(target=consumer,
